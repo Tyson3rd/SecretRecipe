5112709a75345207015022a1df41c5e247054dff
Liem <25380136+jjliem@users.noreply.github.com>
2021-11-11 15:44:48 -0600

    adds menu cards

--- server/models/index.js
+++ server/models/index.js
@@ -1,40 +1,44 @@
-const {Sequelize, DataTypes, Model} = require('sequelize')
-const {sequelize} = require('../db')
-const bcrypt = require('bcrypt')
-
+const { Sequelize, DataTypes, Model } = require("sequelize");
+const { sequelize } = require("../db");
+const bcrypt = require("bcrypt");
 
 class User extends Model {}
 
 class Recipe extends Model {}
 
-User.init({
+User.init(
+  {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
-}, {
+  },
+  {
     sequelize,
     timestamps: false,
-});
+  }
+);
 
 User.beforeCreate((user, options) => {
-    return bcrypt.hash(user.password, 3)
-     .then(hash =>{
-         user.password = hash;
-     })
-     .catch(err => {
-         throw new Error
-     });
+  return bcrypt
+    .hash(user.password, 3)
+    .then((hash) => {
+      user.password = hash;
+    })
+    .catch((err) => {
+      throw new Error();
+    });
 });
 
-
-Recipe.init({
+Recipe.init(
+  {
     name: DataTypes.STRING,
-    ingredients: DataTypes.STRING
-}, {
+    ingredients: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
     sequelize,
     timestamps: false,
-});
+  }
+);
 
-
-
-module.exports = {User, Recipe};
+module.exports = { User, Recipe };
